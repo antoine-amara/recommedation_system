2541a14a51891e8198e44bd5b76cbe35db0722cc
jfvm <jfdurk@gmx.fr>
2016-01-14 16:07:08 +0100

    parseDatas

--- src/DataParser.cpp
+++ src/DataParser.cpp
@@ -11,7 +11,25 @@ DataParser::DataParser(string filename) {
 void DataParser::parse() {
 }
 
-void DataParser::parseDatas() {
+void DataParser::parseDatas(int nbMovies, int nbUsers) {
+
+	int i j;
+	string file = m_filename+".base";
+	ifstream set(file, ios::in)
+	int idM, idU, Mark;
+	gsl_matrix m_datas * = gsl_matrix_alloc(nbMovies,nbUsers);
+
+	for (i = 0; i < nbMovies; i++){
+		for (j=0; j < nbUsers < j++){
+			if (idM == i && idU == j)
+				idM = ;
+				idU = ;
+				gsl_matrix_set(m_datas, i, j, Mark);
+			else
+				gsl_matrix_set(m_datas, i, j, -1);
+		}
+	}
+
 }
 
 void DataParser::parseGenres() {
@@ -30,4 +48,5 @@ vector<string> DataParser::getMovies() {
 }
 
 DataParser::~DataParser() {
+	gsl_matrix_free(m_datas);
 }
