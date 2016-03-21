434fbfb2376456aad785f281e566df7545cd03cd
jfvm <jfdurk@gmx.fr>
2016-03-21 18:06:05 +0100

    debug validator suite

--- src/Validator.cpp
+++ src/Validator.cpp
@@ -5,13 +5,13 @@ using namespace std;
 Validator::Validator(){
 	this->m_nbTestSets = 5;
 	this->m_movieRecommender = MovieRecommender("u");
-	//this->m_errors = vector<int>();
+	this->m_errors = vector<int>();
 }
 
 Validator::Validator(string filename, int nbTestSets){
 	this->m_nbTestSets = nbTestSets;
 	this->m_movieRecommender = MovieRecommender(filename);
-	//this->m_errors = vector<int>();
+	this->m_errors = vector<int>();
 }
 
 void Validator::start(){
@@ -22,7 +22,7 @@ void Validator::start(){
 }
 
 void Validator::computeError(string dataset){
-	DataParser d = new DataParser(this->m_movieRecommender.getX().size2, this->m_movieRecommender.getTheta().size2);
+	DataParser d = new DataParser(this->m_movieRecommender.getX()->size2, this->m_movieRecommender.getTheta()->size2);
 	Vector3 dTest = d.parseTest();
 	int errorCount = 0;
 
@@ -38,7 +38,7 @@ void Validator::computeError(string dataset){
 }
 
 int Validator::computeGlobalError(){
-	int taille = this.m_errors.size();
+	int taille = this->m_errors.size();
 	int GlobalError = accumulate(this->m_errors.begin(), this->m_errors.end(),0);
 	GlobalError /= taille;
 	
@@ -46,13 +46,13 @@ int Validator::computeGlobalError(){
 }
 
 void printReport(){
-	for(unsigned int i = 0; i < m_errors.size(); i++){
+	for(unsigned int i = 0; i < this->m_errors.size(); i++){
 		cout << "DataSet :" << i;
-		cout << "Error :" << m_errors[i] << endl;
+		cout << "Error :" << this->m_errors[i] << endl;
 	}
 	cout << endl;
-	cout << "TotalDataSet :" << m_nbTestSets;
-	cout << "GlobalError :" << computeGlobalError << endl;
+	cout << "TotalDataSet :" << this->m_nbTestSets;
+	cout << "GlobalError :" << computeGlobalError() << endl;
 
 }
 
